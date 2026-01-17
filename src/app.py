254bcbd28368409c5fe38f99f51a5260b9d0f3b0
Busra Akkan <busrakkan@gmail.com>
2026-01-17 12:43:36 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -21,18 +21,15 @@ identify, prioritize, and mitigate security risks using structured methodologies
 """)
 
 
-# Load Data
 df_assets = load_assets("data/assets.csv")
 stride_threats = load_stride_threats("data/STRIDE_threats.csv")
 df_threats = link_threats_to_assets(df_assets, stride_threats)
 df_capec = pd.read_csv("data/CAPEC.csv")
 df_controls = pd.read_csv("data/ISO_controls.csv")
 
-# Calculate Risk
 df_risk = calculate_risk(df_assets, df_threats, df_capec)
 df_controls_suggested = suggest_controls(df_risk, df_controls)
 
-# Sidebar Filters
 st.sidebar.header("Filters")
 
 risk_level_filter = st.sidebar.multiselect(
@@ -44,7 +41,6 @@ risk_level_filter = st.sidebar.multiselect(
 df_risk_filtered = df_risk[df_risk["Risk_Level"].isin(risk_level_filter)]
 
 
-# Tabs
 tab1, tab2, tab3 = st.tabs(["Risk Table", "Risk Visualization", "Controls"])
 
 with tab1:
@@ -63,7 +59,6 @@ with tab3:
     st.dataframe(df_controls_suggested, use_container_width=True)
 
 
-# Downloads
 st.sidebar.header("Downloads")
 
 st.sidebar.download_button(
