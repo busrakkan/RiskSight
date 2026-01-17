7fda895fe77cfe51cf7bc7d67b395bbd2a92a7e5
Busra Akkan <busrakkan@gmail.com>
2026-01-17 12:44:11 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,19 +8,15 @@ from risk_register import generate_risk_register
 from visualization import plot_risk_matrix, plot_top_risks
 
 
-# Load assets
 df_assets = load_assets("data/assets.csv")
 
 
-# Load STRIDE threats and link to assets
 stride_threats = load_stride_threats("data/STRIDE_threats.csv")
 df_threats = link_threats_to_assets(df_assets, stride_threats)
 
-# Load CAPEC feasibility
 df_capec = pd.read_csv("data/CAPEC.csv")
 
 
-# Calculate Risk
 df_risk = calculate_risk(df_assets, df_threats, df_capec)
 df_risk.to_csv("outputs/risk_report.csv", index=False)
 
@@ -28,7 +24,6 @@ print("\n=== Risk Table ===")
 print(df_risk)
 
 
-# Load ISO Controls & suggest mitigations
 df_controls = pd.read_csv("data/ISO_controls.csv")
 df_controls_suggested = suggest_controls(df_risk, df_controls)
 df_controls_suggested.to_csv("outputs/controls_report.csv", index=False)
@@ -37,12 +32,10 @@ print("\n=== Suggested Controls ===")
 print(df_controls_suggested)
 
 
-# Visualize Risk Matrix
 plot_risk_matrix(df_risk)
 plot_top_risks(df_risk)
 
 
-# Generate Risk Register
 df_risk_register = generate_risk_register(df_risk, df_controls)
 df_risk_register.to_csv("outputs/risk_register.csv", index=False)
 
